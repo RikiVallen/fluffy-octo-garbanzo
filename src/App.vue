ce0b1b756f79606ab273ec2a74460bfdb37ea31a
Riki Vallen <riki@madeinkatana.com>
2023-01-09 08:37:56 +1030

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <HelloWorld :tracks="tracks" />
+</template>
+
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core';
+import HelloWorld from './components/HelloWorld.vue'
+
+interface Track {
+	id: number,
+	name: string,
+	artist: string,
+	album: string
+}
+
+const tracks = useLocalStorage<Track[]>('tracks', []);
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+	<div v-for="track in tracks" :key="track.id"></div>
+</template>
+
+<script lang="ts" setup>
+interface Track {
+	id: number,
+	name: string,
+	artist: string,
+	album: string
+}
+
+defineProps<{
+	tracks: Track
+}>()
+</script>
